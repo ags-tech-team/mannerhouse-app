7b4d08047da85f283789044735c3642bcba7456d
Arthur Ribeiro Goncalves <ribeirogon.arthur@gmail.com>
2026-08-10 20:08:40 -0300

    arrumando backend

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -23,29 +23,32 @@ const models = {
 
 const syncDatabase = async () => {
   try {
-    await User.sync();
+    await User.sync({ alter: true });
     console.log('✅ User table sync');
-    
-    await Barber.sync();
+
+    await Barber.sync({ alter: true });
     console.log('✅ Barber table sync');
-    
-    await Client.sync();
+
+    await Client.sync({ alter: true });
     console.log('✅ Client table sync');
-    
-    await Product.sync();
+
+    await Product.sync({ alter: true });
     console.log('✅ Product table sync');
-    
-    await Expense.sync();
+
+    await Expense.sync({ alter: true });
     console.log('✅ Expense table sync');
-    
-    await CashRegister.sync();
+
+    await CashRegister.sync({ alter: true });
     console.log('✅ CashRegister table sync');
-    
-    await Appointment.sync();
+
+    await Appointment.sync({ alter: true });
     console.log('✅ Appointment table sync');
-    
-    await Revenue.sync();
+
+    await Revenue.sync({ alter: true });
     console.log('✅ Revenue table sync');
+
+    await Sale.sync({ alter: true });
+    console.log('✅ Sale table sync');
 
     console.log('📦 Banco de dados sincronizado com sucesso!');
   } catch (error) {
